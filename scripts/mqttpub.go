73785c2ed74994914fc7330f78ae3adfb2a3bff6
Teemu <56117294+teemvil@users.noreply.github.com>
2023-03-02 11:03:36 +0200

    Create mqttpub.go

---
+++ scripts/mqttpub.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+
+	jwt "github.com/golang-jwt/jwt"
+)
+
+func main() {
+
+	claims := jwt.MapClaims{
+		"subject": "test",
+		"name":    "name",
+		"another": "thing",
+	}
+
+	secret := []byte("secret")
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signedToken, err := jwtToken.SignedString(secret)
+
+	fmt.Println("signed token: ", signedToken)
+
+	opts := MQTT.NewClientOptions()
+	opts.AddBroker("192.168.0.24:1883")
+
+	client := MQTT.NewClient(opts)
+
+	if mqttToken := client.Connect(); mqttToken.wait() && mqttToken.Error() != nil {
+		fmt.Println(mqttToken.Error())
+		os.Exit(1)
+	}
+
+	mqttToken := client.Publish("test-channel", 0, false, signedToken)
+	mqttToken.Wait()
+
+}
